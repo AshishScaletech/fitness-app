9aa0a6a520a9a8328a18ce3158d23e67951e340d
AshishScaletech <ashish.patel@scaletech.xyz>
2023-06-14 16:08:10 +0530

    dashboard design

--- src/features/fitness/component/dashboard.tsx
+++ src/features/fitness/component/dashboard.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { IActivity, IFitness } from '../interface/interface';
+import ReactApexChart from 'react-apexcharts';
+
+import ProfileImg from '../../../assets/images/profile-img.jpg';
 
 interface IProps {
 	fitnessActivity: IFitness;
@@ -10,8 +13,112 @@ const Dashboard: React.FC<IProps> = (props) => {
 	const Activities = () => {
 		fitnessActivity.activities.map((activity: IActivity) => activity);
 	};
+	// const options: any = {
+	// 	chart: {
+	// 		type: 'radialBar',
+	// 		offsetY: -20,
+	// 		sparkline: {
+	// 			enabled: true
+	// 		}
+	// 	},
+	// 	plotOptions: {
+	// 		radialBar: {
+	// 			startAngle: -90,
+	// 			endAngle: 90,
+	// 			track: {
+	// 				background: '#e7e7e7',
+	// 				strokeWidth: '97%',
+	// 				margin: 5, // margin is in pixels
+	// 				dropShadow: {
+	// 					enabled: true,
+	// 					top: 2,
+	// 					left: 0,
+	// 					color: '#999',
+	// 					opacity: 1,
+	// 					blur: 2
+	// 				}
+	// 			},
+	// 			dataLabels: {
+	// 				name: {
+	// 					show: false
+	// 				},
+	// 				value: {
+	// 					offsetY: -2,
+	// 					fontSize: '22px'
+	// 				}
+	// 			}
+	// 		}
+	// 	},
+	// 	grid: {
+	// 		padding: {
+	// 			top: -10
+	// 		}
+	// 	},
+	// 	fill: {
+	// 		type: 'gradient',
+	// 		gradient: {
+	// 			shade: 'light',
+	// 			shadeIntensity: 0.4,
+	// 			inverseColors: false,
+	// 			opacityFrom: 1,
+	// 			opacityTo: 1,
+	// 			stops: [0, 50, 53, 91]
+	// 		}
+	// 	},
+	// 	labels: ['Average Results']
+	// };
+	// const series = [76];
 	console.log('Activities:', Activities);
-	return <div>Fitness Activities</div>;
+	return (
+		<div className='width--full dashboard-wrapper flex'>
+			<div className='favorite-wrapper width--70 '>
+				<p className='mb--16 font-size--24 font--medium'>Fitness Activity</p>
+				<p className='font-size--default mb--35 text--grey-100 font--light'>
+					Physical activity is defined as any bodily movement produced by skeletal muscles that results in
+					energy expenditure. The energy expenditure can be measured in kilocalories. Physical activity in
+					daily life can be categorized into occupational, sports, conditioning, household, or other
+					activities.
+				</p>
+				<div className='favorite width--full'>
+					<p className='flex align-items--center text--grey-600 mb--20'>
+						{' '}
+						<span className='width--7px height--20px bg--grey-150 mr--15 ' />
+						Favorite
+					</p>
+					<div className='flex justify-content--between'>
+						<div className='heart-chart width--32'>heart</div>
+						<div className='heart-chart width--32'>calorie</div>
+						<div className='heart-chart width--32'>distance</div>
+					</div>
+				</div>
+				<div className='activity-wrapper width--full'>
+					<div className='activity'>
+						<p>Activity</p>
+						<div className='flex justify-content--between'>
+							<div className='activity-detail width--58'>chart</div>
+							<div className='activity-list width--40'>list of activity</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='profile width--30 bg--white p--20'>
+				<p className='flex align-items--center text--grey-600 mb--20'>
+					{' '}
+					<span className='width--7px height--20px bg--grey-150 mr--15 ' />
+					profile
+				</p>
+				<div className='flex align-items--center justify-content--center width--full'>
+					<div className='profile-img__wrapper border-radius--half '>
+						<img
+							src={ProfileImg}
+							alt='profile'
+							className='width--full height--full border-radius--half object-fit--cover'
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Dashboard;
--- src/shared/components/header/sidebar.tsx
+++ src/shared/components/header/sidebar.tsx
@@ -1,42 +1,19 @@
 import React, { ReactNode, useState } from 'react';
 // import { FaTh, FaBars, FaUserAlt, FaRegChartBar, FaCommentAlt, FaShoppingBag, FaThList } from 'react-icons/fa';
 import './sidebar.scss';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { Dashboard, Fitness } from '../icons/icons';
 
 const Sidebar: React.FC = () => {
 	const [isOpen, setIsOpen] = useState(true);
 	const toggle = () => setIsOpen(!isOpen);
+
+	const navigate = useNavigate();
 	const menuItem = [
 		{
 			path: '/',
 			name: 'Dashboard',
 			icon: <Dashboard className='width--20px height--20px ' />
-		},
-		{
-			path: '/about',
-			name: 'About',
-			icon: 'icon-dashboard'
-		},
-		{
-			path: '/analytics',
-			name: 'Analytics',
-			icon: 'icon-dashboard'
-		},
-		{
-			path: '/comment',
-			name: 'Comment',
-			icon: 'icon-dashboard'
-		},
-		{
-			path: '/product',
-			name: 'Product',
-			icon: 'icon-dashboard'
-		},
-		{
-			path: '/productList',
-			name: 'Product List',
-			icon: 'icon-dashboard'
 		}
 	];
 
@@ -45,26 +22,25 @@ const Sidebar: React.FC = () => {
 		color: '#000'
 	};
 	return (
-		<div style={{ width: isOpen ? '200px' : '70px' }} className='sidebar'>
-			<div className='top_section'>
-				<p style={{ display: isOpen ? 'block' : 'none' }} className='logo'>
-					Fitness
-				</p>
-				<div style={{ marginLeft: isOpen ? '50px' : '0px' }} className='ml--15'>
-					<p onClick={toggle}>
-						<Fitness className='width--50px height--40px' />
-					</p>
-				</div>
+		<div
+			style={{ width: isOpen ? '200px' : '70px' }}
+			className='sidebar border--gry-50 flex align-items--center flex--column justify-content--start'
+		>
+			<div className='top_section flex align-items--center justify-content--center' onClick={toggle}>
+				<Fitness className='width--50px height--40px' />
 			</div>
 			{menuItem.map((item: any, index: number) => (
 				<NavLink
 					to={item.path}
 					key={index}
-					className='link text--black'
+					onClick={() => {
+						navigate('/navigate');
+					}}
+					className='link flex justify-content--center'
 					style={({ isActive }) => (isActive ? isActiveStyle : {})}
 				>
-					<div className='icon'>{item.icon}</div>
-					<div style={{ display: isOpen ? 'block' : 'none' }} className='text--black'>
+					<div className='icon st'>{item.icon}</div>
+					<div style={{ display: isOpen ? 'block' : 'none' }} className='width--full'>
 						{item.name}
 					</div>
 				</NavLink>
